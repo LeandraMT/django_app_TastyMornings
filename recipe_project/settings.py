4a5d35c848e464f209ffc06bb69fe135646be63b
LeandraMT <magantierleandra@gmail.com>
2024-01-12 22:58:11 +0100

    updated new name of app

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -23,7 +23,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "mysterious-fortress-53629-15fc70f8a464.herokuapp.com",
+    "tastymornings-ae401e6582be.herokuapp.com",
 ]
 
 
