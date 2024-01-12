938e77dfa96682f3ee4bf1d5b19feae80e8c7300
LeandraMT <magantierleandra@gmail.com>
2024-01-12 22:47:46 +0100

    debugging issues with static files

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -134,7 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR.parent, "static")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "profiles", "static"),
+    os.path.join(BASE_DIR, "recipes", "static"),
+]
+
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
