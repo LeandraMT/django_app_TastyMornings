aee1b79000c0ce4e7ad5344832c1c01d6279ce58
LeandraMT <magantierleandra@gmail.com>
2024-01-13 01:07:26 +0100

    added more lines of code to debug issue with static files

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -16,4 +16,6 @@ urlpatterns = [
 ]
 
 # Corrected pattern for serving media files during development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
